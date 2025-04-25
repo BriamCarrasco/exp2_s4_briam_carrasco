16e513a5559377a26658b3c70301de1b794496a0
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-04-24 20:55:23 -0400

    Se agrega 'extends RepresentationModel<Movies>' para implementacion de HATEOAS

--- src/main/java/com/movies/exp2_s4_briam_carrasco/model/Movies.java
+++ src/main/java/com/movies/exp2_s4_briam_carrasco/model/Movies.java
@@ -1,5 +1,7 @@
 package com.movies.exp2_s4_briam_carrasco.model;
 
+import org.springframework.hateoas.RepresentationModel;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,7 +11,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "TB_MOVIES")
-public class Movies {
+public class Movies extends RepresentationModel<Movies> {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
