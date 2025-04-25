40a1344e46102c59fb0fccc4ff322473ba765cc4
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-04-24 21:43:05 -0400

    Se modifica controller para el uso de hateoas

--- src/main/java/com/movies/exp2_s4_briam_carrasco/controller/MoviesController.java
+++ src/main/java/com/movies/exp2_s4_briam_carrasco/controller/MoviesController.java
@@ -2,8 +2,12 @@ package com.movies.exp2_s4_briam_carrasco.controller;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,18 +29,53 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RequestMapping("/movies")
 @CrossOrigin(origins = "*")
 public class MoviesController {
+
     @Autowired
     private MoviesService moviesService;
 
     @GetMapping
+    public CollectionModel<EntityModel<Movies>> getAllMovies()
+    {
+        List<Movies> movies = moviesService.getAllMovies();
+        
+        List<EntityModel<Movies>> moviesResources = movies.stream()
+        .map(movie -> EntityModel.of(movie,
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getMovieById(movie.getId())).withSelfRel()))
+                .collect(Collectors.toList());
+        
+        WebMvcLinkBuilder linkTO = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllMovies());
+        CollectionModel<EntityModel<Movies>> resources = CollectionModel.of(moviesResources, linkTO.withRel("movies"));
+            
+        return resources;
+    }
+
+
+    @GetMapping("/{id}")
+    public EntityModel<Movies> getMovieById(@PathVariable long id)
+    {
+        Optional<Movies> movie = moviesService.getMovieById(id);
+        if(movie.isPresent())
+        {
+            return EntityModel.of(movie.get(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getMovieById(id)).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllMovies()).withRel("all-movies"));
+        }else
+        {
+            throw new MovieNotFoundException("Movie not found with id: " + id);
+        }
+    }
+    /* 
+    @GetMapping
     public List<Movies> getAllMovies() {
         return moviesService.getAllMovies();
     }
+    
 
     @GetMapping("/{id}")
     public Optional<Movies> getMovieById(@PathVariable Long id){
         return moviesService.getMovieById(id);
     }
+    */
 
     @PostMapping
     public Movies createMovies(@RequestBody Movies movie) {
