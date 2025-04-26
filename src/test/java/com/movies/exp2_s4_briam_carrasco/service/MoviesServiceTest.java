8eb5171b4a7796fbf150c5f2432708638d4cc4af
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-04-25 20:25:09 -0400

    Se agrega test updateMovie

--- src/test/java/com/movies/exp2_s4_briam_carrasco/service/MoviesServiceTest.java
+++ src/test/java/com/movies/exp2_s4_briam_carrasco/service/MoviesServiceTest.java
@@ -1,9 +1,15 @@
 package com.movies.exp2_s4_briam_carrasco.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -22,22 +28,52 @@ public class MoviesServiceTest {
     @Mock
     private MoviesRepository moviesRepositoryMock;
     
+    private Movies movies;
+
+    @BeforeEach
+    void setUp() {
+        movies = new Movies();
+        movies.setTitulo("Shrek");
+        movies.setAnio(2001L);
+        movies.setDirector("Andrew Adamson");
+        movies.setGenero("Animacion/comedia");
+        movies.setSinopsis("Un ogro llamado Shrek debe rescatar a una princesa para recuperar la paz de su pantano");
+    }
+
+    @AfterEach
+    void tearDown() {
+        movies = null;
+    }
+
+
     @Test
     public void createMovieTest(){
-        Movies movies = new Movies();
-        movies.setTitulo("Inception");
-        movies.setAnio(2010L);
-        movies.setDirector("Christopher Nolan");
-        movies.setGenero("Sci-Fi");
-        movies.setSinopsis("Un experto en robar secretos a través de los sueños debe implantar una idea en la mente de un CEO");
-        
-
+       
         when(moviesRepositoryMock.save(any())).thenReturn(movies);
 
         Movies resultado = moviesServiceImpl.CreateMovie(movies);
 
-        assertEquals("Inception", resultado.getTitulo());
+        assertEquals("Shrek", resultado.getTitulo());
         
     }
+
+    @Test
+    public void updateMovieTest() {
+        Long movieId = 1L;
+
+        when(moviesRepositoryMock.existsById(movieId)).thenReturn(true);
+        when(moviesRepositoryMock.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
+
+        Movies result = moviesServiceImpl.updateMovie(movieId, movies);
+
+        assertNotNull(result);
+        assertEquals(movieId, result.getId());
+        assertEquals("Shrek", result.getTitulo());
+        assertEquals(2001L, result.getAnio());
+        assertEquals("Andrew Adamson", result.getDirector());
+        assertEquals("Animacion/comedia", result.getGenero());
+        assertEquals("Un ogro llamado Shrek debe rescatar a una princesa para recuperar la paz de su pantano", result.getSinopsis());
+    }
+
     
 }
